cb7c0686dff886f27637c13488efae1d75ec3764
DarkSwag81 <quentin.louis@sfr.fr>
2016-11-16 23:09:44 +0100

    inb4karaAtoutcassélesInputs

--- DSHJAM2016/Assets/persos/CharacterController.cs
+++ DSHJAM2016/Assets/persos/CharacterController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TEST : MonoBehaviour {
+public class CharacterController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
