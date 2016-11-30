b05b1f414248846b5953d715753d5415b785c8a9
Karamaz0V1 <florent.guiotte@gmail.com>
2016-11-30 16:06:25 +0100

    WIP skins

--- DSHJAM2016/Assets/persos/CharacterSelect.cs
+++ DSHJAM2016/Assets/persos/CharacterSelect.cs
@@ -22,7 +22,8 @@ public class CharacterSelect : MonoBehaviour
 
     private int nbPlayers = 0;
     private static int nbMaxPlayers = 4;
-	private int[] slots;
+	private bool[] slots;
+	private int[] skins;
 
 	private List<Player> players;
 
@@ -30,9 +31,10 @@ public class CharacterSelect : MonoBehaviour
     void Start()
     {
 		players = new List<Player>(nbMaxPlayers); // I hate stacks
-		slots = new int[nbMaxPlayers];
-		for ( int i = 0; i < nbMaxPlayers;i++ ) // I hate C#
-			slots[i] = -1;
+		slots = new bool[nbMaxPlayers];
+		skins = new int[nbMaxPlayers];
+		//for ( int i = 0; i < nbMaxPlayers;i++ ) // I hate C#
+		//	slots[i] = -1;
 
 		graphics = Resources.LoadAll<Sprite>(graphic.name);
     }
@@ -51,6 +53,12 @@ public class CharacterSelect : MonoBehaviour
 			if (Input.GetButtonDown ("JS" + "Item" + i))
 				delPlayerJS (i);
 
+		// Listen for move on JS
+		for (int i = 0; i < nbMaxPlayers; i++) {
+			float axis = Input.GetAxis("JS" + "Move" + i);
+			if (axis == 0) continue;
+			//skins [i] = (skins[i] + (int) (axis > 0) - (int) (axis < 0)) % (skins.Length);
+		}
 	
         if (Input.GetButtonDown("Submit"))
         {
@@ -58,31 +66,35 @@ public class CharacterSelect : MonoBehaviour
 
         }
 
+		// Display players and skins
 		for (int i = 0; i < nbMaxPlayers; i++)
-			jx[i].sprite = graphics[slots[i]+1];//numbers [nbPlayers];
-		GetComponent<SpriteRenderer> ().sprite = numbers [nbPlayers];
-			
+			if (!slots[i]) jx[i].sprite = graphics[0];
+			else jx[i].sprite = graphics[skins[i]];			
     }
+
+	void addPlayer(string pad, int id=0) {
+	}
 
 	void addPlayerJS(int id=0) {
 		// First, check slot status
-		if (slots[id] != -1) return;
+		if (slots[id]) return;
 
 		// Then create a player with his own JS, shitty gameplay will come soon
-		players.Add(new Player(nbPlayers, new LeftHand("JS",id.ToString()), new RightHand("JS",id.ToString())));
-		slots [id] = nbPlayers;
+		//players.Add(new Player(nbPlayers, new LeftHand("JS",id.ToString()), new RightHand("JS",id.ToString())));
+		slots [id] = true;
+		skins [id] = Random.Range (1, graphics.Length);
 		nbPlayers++;
-		Debug.Log ("New player with " + Input.GetJoystickNames()[id] + "! Current players : " + nbPlayers);
+		Debug.Log ("New player with " + Input.GetJoystickNames()[id] + "! Current players : " + nbPlayers + " " + players.Count + "[" + slots[0] + ","+ slots[1]+ "," + slots[2]+ ","+ slots[3] +"]");
 	}
 
 	void delPlayerJS(int id=0) {
 		// First, check slot status
-		if (slots[id] == -1) return;
+		if (!slots[id]) return;
 
 		// Then delete a player with his JS id
-		players.RemoveAt(slots[id]);
-		slots [id] = -1;
+		//players.RemoveAt(slots[id]);
+		slots [id] = false;
 		nbPlayers--;
-		Debug.Log ("Player abandon :( Current players : " + nbPlayers);
+		Debug.Log ("Player abandon :( Current players : " + nbPlayers + " " + players.Count + "[" + slots[0] + "," + slots[1] + ","+ slots[2]+ ","+ slots[3] +"]");
 	}
 }
